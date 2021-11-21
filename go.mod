8c1afe93fa0947ee1530c395e288d735f9820354
Ingrid <git@ingrids.email>
2021-11-21 22:26:32 +0100

    backend: setup chi and postgres

---
+++ go.mod
@@ -0,0 +1,8 @@
+module git.sr.ht/~intarga/otq-no
+
+go 1.16
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+)
--- server.go
+++ server.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v4"
 )
 
+func root(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello root"))
+}
+
 func main() {
-	fmt.Println("Serve!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	r.Get("/", root)
+
+	http.ListenAndServe(":8080", r)
 }
